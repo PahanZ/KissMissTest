e8459b00e0a635450347c3e361acee20807939aa
PahanZ <you@example.com>
2017-08-08 20:40:01 +0300

    add foreach and map

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let start = 1; 
 let result = []; 
 while(start<=100) { 
@@ -34,3 +35,67 @@ result[i]='kiss';
 console.log(result); 
 }; 
 setTimeout(kiss, 50); 
+
+
+//-----------------second option------------------// 
+
+// let start = 1; 
+// let result = []; 
+// while(start<=100) { 
+// result.push(start++) 
+// }; 
+
+
+// let x = () => { 
+// result.forEach(function(element, index, result){ 
+// if (result[index] % 5 ==0 && result[index] % 3 ==0) { 
+// result[index] ='kissmiss'; 
+// } 
+// }) 
+// console.log(result) 
+// }; 
+// setTimeout(x, 50); 
+
+
+// let y = () => { 
+// result.forEach(function(element, index, result){ 
+// if (result[index] % 3 == 0) { 
+// result[index] ='miss'; 
+// } 
+// }) 
+// console.log(result) 
+// }; 
+// setTimeout(y, 50); 
+
+// let z = () => { 
+// result.forEach(function(element, index, result){ 
+// if (result[index] % 5 == 0) { 
+// result[index] ='kiss'; 
+// } 
+// }) 
+// console.log(result) 
+// }; 
+// setTimeout(z, 50); 
+
+
+//-----------------third option(without deelay)------------------// 
+
+// let start = 1; 
+// let result = []; 
+// while(start<=100) { 
+// result.push(start++) 
+// }; 
+
+
+// let n = () => { 
+// result.map(function(e){ 
+// if(e % 3 == 0 && e % 5 == 0){ 
+// e='misskiss'; 
+// } else if (e % 3 == 0){ 
+// e='miss'; 
+// } else if(e % 5 == 0) { 
+// e='kiss'; 
+// } 
+// console.log(e) 
+// })}; 
+// setTimeout(n, 50); 
